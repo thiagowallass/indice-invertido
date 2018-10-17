e1cbe4ec6da597843da9b82300bcd61031388dff
arthurgsf <arthur.gsf.gsf@gmail.com>
2018-10-17 18:40:34 -0300

    starting point

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include "include/lista.h"
+#include <string.h>
+#include "include/inverted-list.h"
+
+int main(){
+    inv_list * invl;
+    invl = ilCreate("files/");
+    return 0;
+}
